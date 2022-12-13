a73334e2f5b0c38ec9340a0ccf9476fe3bdb33e2
Eddy Ndizera <eddy.ndizera@uclouvain.be>
2022-12-13 21:23:03 +0100

    add static config

--- ittakesavillage/settings.py
+++ ittakesavillage/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Media
 MEDIA_ROOT = BASE_DIR / 'uploads/'
