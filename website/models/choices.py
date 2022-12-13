d0da092a7335886b6fb86f2d9d74d49a30066183
Eddy <eddyndizera@gmail.com>
2022-12-13 23:11:11 +0100

    modify entrance

--- website/models/choices.py
+++ website/models/choices.py
@@ -15,7 +15,31 @@ class Language(models.TextChoices):
 
 
 class Speciality(models.TextChoices):
-    # SPECIALITY = 'Lactation specialist', _('Lactation specialist'),
     LACTATION = 'Certified Breastfeeding Specialist', _('Certified Breastfeeding Specialist'),
     HYPNO_BIRTHING = 'HypnoBirthing® Childbirth Educator', _('HypnoBirthing® Childbirth Educator')
+    MIDWIFE = 'Midwife', _('Midwife')
+    DOULA = 'Doula', _('Doula')
+    LACTATION_BREASTFEEDING_CONSULTANT = 'Lactation/breastfeeding consultant', _('Lactation/breastfeeding consultant')
+    BABYWEARING_SPECIALIST = 'Babywearing specialist', _('Babywearing specialist')
+    SLEEP_SPECIALIST = 'Sleep specialist', _('Sleep specialist')
+    PEDIATRICIAN = 'Pediatrician', _('Pediatrician')
+    PERINATAL_PSYCHOLOGIST = 'Perinatal psychologist', _('Perinatal psychologist')
+    PHYSICAL_THERAPIST = 'Physical therapist', _('Physical therapist')
+    PERIADTRICIAN = 'Periadtrician', _('Periadtrician')
+    PRE_POST_NATAL_YOGA_TEACHER = 'Pre/post natal yoga teacher', _('Pre/post natal yoga teacher')
+    PRE_POST_NATAL_MASSAGE = 'Pre/post natal massage', _('Pre/post natal massage')
+    BABY_MASSAGE = 'Baby massage', _('Baby massage')
+    PRE_POST_NATAL_PHYSICAL_THERAPIST = 'Pre/post natal physical therapist', _('Pre/post natal physical therapist')
+    OSTEOPATH = 'Osteopath', _('Osteopath')
+    PARENTING_COACH = 'Parenting coach', _('Parenting coach')
+    PSYCHOMOTRICIAN = 'Psychomotrician', _('Psychomotrician')
 
+    __empty__ = ''
+
+
+class City(models.TextChoices):
+    BRUSSELS = 'Brussels', _('Brussels')
+    ANTWERPEN = 'Antwerpen', _('Antwerpen')
+    GHENT = 'Ghent', _('Ghent')
+
+    __empty__ = ''
--- website/views/parent/home.py
+++ website/views/parent/home.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
 
+from website.forms.parent.specialists import SearchSpecialistForm
+
 
 class HomeView(TemplateView):
     template_name = "website/parent/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = SearchSpecialistForm()
+        return context
